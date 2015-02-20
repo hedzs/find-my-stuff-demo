f53b331f2d3a07578961bc548e83a0564457fef8
Sinka Zoltán <hedzs@catv-5-148-220-20.catv.broadband.hu>
2015-02-20 09:33:36 +0100

    Initial Commit

---
+++ FindMyStuff/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FindMyStuff
+//
+//  Created by Sinka Zoltán on 2015. 02. 20..
+//  Copyright (c) 2015. Sinka Zoltán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
